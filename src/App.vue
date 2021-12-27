1f15dbd36ac44e3b43bacdf99ca0dace322e8957
1016950775 <1016950775@qq.com>
2021-12-27 17:39:32 +0800

    12.27

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png">
 		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		
 		<h1 style="text-align: left;">步进轮播</h1>
 		<swiperStep :data="swiperData" :imgNum="imgNum"></swiperStep>
 	</div>
